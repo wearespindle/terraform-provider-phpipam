f8c53c94ab7de49082a8188bce64346e9a97eb5d
Jos van Bakel <jos@codeaddict.org>
2020-03-24 10:47:56 +0100

    Fix tests

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/hashicorp/terraform/terraform"
 	"github.com/wearespindle/terraform-provider-phpipam/plugin/providers/phpipam"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return phpipam.Provider()
-		},
+		ProviderFunc: phpipam.Provider,
 	})
 }
